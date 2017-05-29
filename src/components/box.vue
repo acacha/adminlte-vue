e2a9b32ad118ca379c5690f01c3cc711ad70b70e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-05-29 17:51:01 +0200

    First version with only box component

--- src/components/box.vue
+++ src/components/box.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="box box-solid" :class="[{'collapsed-box' : isCollapsed  }, 'box-' + color]">
-        <div class="box-header with-border">
+    <div class="box" :class="[{'box-solid' : isSolid}, {'collapsed-box' : isCollapsed  }, 'box-' + color]">
+        <div class="box-header" :class="{'with-border' : headerHaveBorder}">
             <div class="box-title">
                 <slot name="title">Put your title here using slot with name title</slot>
             </div>
             <div class="box-tools pull-right">
                 <slot name="box-tools">
-                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa" :class="collapseIcon"></i></button>
+                    <button v-if="isRemovable" type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    <button v-if="isCollapsable" type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa" :class="collapseIcon"></i></button>
                 </slot>
             </div>
         </div>
         <div class="box-body">
             <slot>Put your content here using default slot</slot>
+        </div>
+        <div class="box-footer" v-if="hasFooterSlot()">
+            <slot name="footer">This is the footer!</slot>
         </div>
         <div class="overlay" v-if="isLoading">
             <i class="fa fa-refresh fa-spin"></i>
@@ -25,7 +28,11 @@
     data() {
       return {
         isCollapsed : this.collapsed,
-        isLoading: this.loading
+        isCollapsable: this.collapsable,
+        isRemovable: this.removable,
+        isLoading: this.loading,
+        isSolid: this.solid,
+        headerHaveBorder: this.borderHeader
       }
     },
     computed: {
@@ -38,17 +45,65 @@
         type: Boolean,
         default: false
       },
+      collapsable: {
+        type: Boolean,
+        default: true
+      },
+      removable: {
+        type: Boolean,
+        default: true
+      },
       loading: {
         type: Boolean,
         default: false
       },
+      solid: {
+        type: Boolean,
+        default: false
+      },
+      borderHeader: {
+        type: Boolean,
+        default: true
+      },
       color: {
         type: String,
-        default: 'primary',
+        default: 'default',
         validator: value => {
           let valid = ['','primary','info', 'danger', 'warning', 'success','default']
           return valid.includes(value)
         }
+      }
+    },
+    methods: {
+      hasFooterSlot() {
+        return !!this.$slots['footer']
+      },
+      load() {
+        this.isLoading = true
+      },
+      unload() {
+        this.isLoading = false
+      },
+      collapse() {
+        this.isCollapsed = true
+      },
+      uncollapse() {
+        this.isCollapsed = false
+      },
+      expand() {
+        this.isCollapsed = false
+      },
+      unExpand() {
+        this.isCollapsed = false
+      },
+      toogleCollapse() {
+        this.isCollapsed = ! this.isCollapsed
+      },
+      toogleExpand() {
+        this.isCollapsed = ! this.isCollapsed
+      },
+      remove() {
+        // TODO
       }
     }
   }
