862aab21df17f44a57d53b44fce247e76e75e8d6
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-12-13 12:46:44 +0100

    Sync

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ function getPlugins () {
 
   plugins.push(new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': process.env.NODE_ENV
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }
   }))
 
