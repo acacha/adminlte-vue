c8763a76f4aa75cc78dcc8c2c81c04bc6b685705
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-12-15 16:14:34 +0100

    Sync

--- src/components/mixins/flash.js
+++ src/components/mixins/flash.js
@@ -6,6 +6,12 @@ export default {
       } else {
         this.showAlert(message, title, color, icon)
       }
+    },
+    success (message) {
+      this.flash(message,'Done!','success','check')
+    },
+    error () {
+      this.flash(message,'Error!','danger','ban')
     }
   }
 }
